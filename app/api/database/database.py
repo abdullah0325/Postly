cc1598eb8bbc8ffbce2e0e25790c5a8ce49045d2
Muhammad abdullah <abdullah032518@gamil.com>
2025-05-29 15:41:37 +0500

    3

--- app/api/database/database.py
+++ app/api/database/database.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
-from contextlib import contextmanager
 
 DATABASE_URL = "postgresql://Postgres_sql_owner:z31bWYweGVFd@ep-white-tree-a7pollpn-pooler.ap-southeast-2.aws.neon.tech/Postgres_sql?sslmode=require"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=1800  # Helps with stale connections
+)
 
-# Dependency for FastAPI
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
