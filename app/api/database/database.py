f66d104247e9f17fa3efe577a90570877e3e5f8e
Muhammad Abdullah <133787552+abdullah0325@users.noreply.github.com>
2025-06-30 01:50:52 +0500

    Update database.py

--- app/api/database/database.py
+++ app/api/database/database.py
@@ -1,22 +1,3 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.orm import Session
-# from contextlib import contextmanager
-
-# DATABASE_URL = ""
-
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# # Dependency for FastAPI
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
